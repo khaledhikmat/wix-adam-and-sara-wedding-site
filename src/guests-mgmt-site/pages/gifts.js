07b3d99f2e18cfb23a192a07f5441eb464ad84ed
Khaled Hikmat <khaled.hikmat@gmail.com>
2020-12-28 21:59:31 -0600

    Gifts changes

--- src/guests-mgmt-site/pages/gifts.js
+++ src/guests-mgmt-site/pages/gifts.js
@@ -2,8 +2,10 @@ import {getGiftsHeaderMessage} from 'backend/settings';
 
 $w.onReady(async function () {
     $w("#txtHeaderMessage").text = await getGiftsHeaderMessage();
-    $w("#creditCardGift").hide();
-	$w("#btnCreditCard").onClick( async (event) =>  {
-        $w("#creditCardGift").isVisible ? $w("#creditCardGift").hide() : $w("#creditCardGift").show();
+    $w("#tblGiftMethods").hide();
+    $w("#giftCreditCards").hide();
+	$w("#btnToggleGiftMethods").onClick( async (event) =>  {
+        $w("#tblGiftMethods").isVisible ? $w("#tblGiftMethods").hide() : $w("#tblGiftMethods").show();
+        $w("#giftCreditCards").isVisible ? $w("#giftCreditCards").hide() : $w("#giftCreditCards").show();
     });
 });
